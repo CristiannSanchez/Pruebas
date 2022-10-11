068dade2bf49508dccc2d5d121bc6bcfaf7c3cba
Grupo #7 <113653406+CristiannSanchez@users.noreply.github.com>
2022-10-10 22:54:26 -0600

    Add files via upload

---
+++ Student.DAL/DBContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using PStudent.DAL.Entities;
+
+namespace PStudent.DAL
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext()
+        {
+        }
+
+        public DBContext(DbContextOptions<DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Registro> Registros { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Registro>(entity =>
+            {
+                entity.ToTable("Registro");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Apellidos)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FehcaDeNacimiento)
+                    .HasColumnType("datetime")
+                    .HasColumnName("Fehca de Nacimiento");
+
+                entity.Property(e => e.Nombres)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Student.DAL/Entities/Registro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PStudent.DAL.Entities
+{
+    public partial class Registro
+    {
+        public int Id { get; set; }
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+        public DateTime? FehcaDeNacimiento { get; set; }
+        public string? Email { get; set; }
+        public int? Telefono { get; set; }
+    }
+}
